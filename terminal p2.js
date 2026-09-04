c6efbd08bd228942f8b134c28299056b1e23f8ba
surya <suryasingh18102007@gmail.com>
2026-09-04 14:38:52 +0530

    add all files

---
+++ terminal p2.js
@@ -0,0 +1,6 @@
+// Basic commands:-
+// 1-clone:cloning oa repository on our local machine(git clone <some link>)
+// 2-status:display the state of the code<git status>
+// 3-add:add is the process before the commit(staged)
+// 4-commit:it is the record of change.<git commi -m"some message"
+// 5-push:it helps to send our loca repo to remote repo<git push origin main>
