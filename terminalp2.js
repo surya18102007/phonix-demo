6e3b876137b9b947958a28b49b77c6c971e22ad3
surya <suryasingh18102007@gmail.com>
2026-09-04 21:10:54 +0530

    one more changes

--- terminalp2.js
+++ terminalp2.js
@@ -5,3 +5,12 @@
 // 4-commit:it is the record of change.<git commi -m"some message"
 // 5-push:it helps to send our loca repo to remote repo<git push origin main>
 // 6-init:it helps  us to make authorization on git command.
+// like if you want to make amother project and want to add and push on other repo so these following commands you need to follow.
+// for adding this point is must to follow.
+// get remote add origin<link>
+// get remote -v(it is for to verify remote)
+// git branch (to check branch):common branch =master
+// git branch -M main(to rebane branch)
+// git push origin main 
+// if do this one time the following step so we don't need to do git push origin main many times.
+// git push -u orgin main
